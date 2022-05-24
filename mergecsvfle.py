f02196fceaf2b4ce675b57d447db25555869c8b7
João Felipe do Nascimento Sampaio <68752870+JoaoFelipeSampaio@users.noreply.github.com>
2022-05-24 17:34:56 -0300

    Update mergecsvfle.py

--- mergecsvfle.py
+++ mergecsvfle.py
@@ -1,25 +1,31 @@
-#arquivo de modelagem 
-
+#Merge CSV file
 import os
 import pandas as pd
 
-
+# You need to set the path where you have put all your archives! 
 path = "/home/joao/Z-Tecs/Modelo/talisma_0/"
 
+#Here you list the files in the directory
 os.listdir(path)
 
-# 2. creates list with files to merge based on name convention
-file_list = [path + f for f in os.listdir(path) if f.startswith('peso')]
-print(file_list)
-#3. creates empty list to include the content of each file converted to pandas DF
+# creates list with files to merge based on name convention
+#choose the initial name of all the files and put in startswith('xxx')
+#keep in mind that all the archives needs to start with the same name or word
+file_list = [path + f for f in os.listdir(path) if f.startswith('peso')] 
+
+# To see all the archives you're importing
+print(file_list) 
+
+# creates empty list to include the content of each file converted to pandas DF
 csv_list = []
  
-# 4. reads each (sorted) file in file_list, converts it to pandas DF and appends it to the csv_list
+# reads each (sorted) file in file_list, converts it to pandas DF and appends it to the csv_list
 for file in sorted(file_list):
     csv_list.append(pd.read_csv(file).assign(File_Name = os.path.basename(file)))
 
-# 5. merges single pandas DFs into a single DF, index is refreshed 
+# merges single pandas DFs into a single DF, index is refreshed 
 csv_merged = pd.concat(csv_list, ignore_index=True)
 
-# 6. Single DF is saved to the path in CSV format, without index column
+# Single DF is saved to the path in CSV format, without index column
 csv_merged.to_csv(path + 'peso_full.csv', index=False)
+
